d9c17fc2d7ee80a7de816911e3cb863f39a67ff4
Vinicius Stock <vinicius.stock@shopify.com>
2025-06-27 16:27:45 -0400

    Allow defining type safe IDs for pools

--- src/pool.rs
+++ src/pool.rs
@@ -1,56 +1,99 @@
 use std::collections::HashMap;
-use std::rc::Rc;
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::marker::PhantomData;
+use std::sync::Arc;
 
 /// A pool of unique items stored by indexed to avoid duplication. Pools can be used to store unique names, like
 /// `Foo::Bar#method`, file paths or any unique value we need to store avoiding cloning and minimizing memory usage.
 ///
-/// We use two hashes with reference counters to guarantee uniqueness without cloning and fast lookups both ways
-pub struct Pool<T: Eq + std::hash::Hash> {
-    id_to_entry: HashMap<usize, Rc<T>>,
-    entry_to_id: HashMap<Rc<T>, usize>,
-    next_id: usize,
+/// We use two hashes with reference counters to guarantee uniqueness without cloning and fast lookups both ways. We
+/// also require defining an ID type for the pool, so that IDs belonging to different pools cannot be mixed up and a
+/// type error will be thrown during compilation.
+///
+/// ```
+/// # #[derive(PartialEq, Debug)]
+/// # pub struct NameId;
+/// # #[derive(PartialEq, Debug)]
+/// # pub struct UriId;
+/// #
+/// # let mut name_pool = Pool::<NameId, String>::new();
+/// # let mut uri_pool = Pool::<UriId, String>::new();
+/// # let name_id = name_pool.add("Foo::Bar#method".into());
+/// # uri_pool.get(&name_id) // Not possible. This will fail to compile because of the ID types
+/// ```
+pub struct Pool<I, T: Eq + Debug + Hash> {
+    id_to_entry: HashMap<u32, Arc<T>>,
+    entry_to_id: HashMap<Arc<T>, u32>,
+    next_id: u32,
+    _phantom: PhantomData<I>,
 }
 
-impl<T: Eq + std::hash::Hash> Pool<T> {
+#[derive(PartialEq, Debug)]
+pub struct PoolId<I: PartialEq> {
+    value: u32,
+    _marker: PhantomData<I>,
+}
+
+impl<I: PartialEq> From<u32> for PoolId<I> {
+    fn from(id: u32) -> Self {
+        Self {
+            value: id,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<I: PartialEq> From<PoolId<I>> for u32 {
+    fn from(id: PoolId<I>) -> Self {
+        id.value
+    }
+}
+
+impl<I: PartialEq, T: Eq + Debug + Hash> Pool<I, T> {
     pub fn new() -> Self {
         Pool {
             id_to_entry: HashMap::new(),
             entry_to_id: HashMap::new(),
             next_id: 0,
+            _phantom: PhantomData,
         }
     }
 
-    pub fn add(&mut self, item: T) -> usize {
+    pub fn add(&mut self, item: T) -> PoolId<I> {
         // If the item already exists, skip adding it and just return its ID
         if let Some(&id) = self.entry_to_id.get(&item) {
-            return id;
+            return id.into();
         }
 
-        let item_rc = Rc::new(item);
+        let item_arc = Arc::new(item);
 
         let id = self.next_id;
-        self.id_to_entry.insert(id, item_rc.clone());
-        self.entry_to_id.insert(item_rc, id);
+        self.id_to_entry.insert(id, item_arc.clone());
+        self.entry_to_id.insert(item_arc, id);
         self.next_id += 1;
-        id
+        id.into()
     }
 
-    pub fn get(&self, id: usize) -> Option<&T> {
-        self.id_to_entry.get(&id).map(|rc| rc.as_ref())
+    pub fn get(&self, id: &PoolId<I>) -> Option<&T> {
+        self.id_to_entry.get(&id.value).map(|rc| rc.as_ref())
     }
 
-    pub fn remove(&mut self, id: usize) {
-        if let Some(entry) = self.id_to_entry.remove(&id) {
+    pub fn remove(&mut self, id: &PoolId<I>) -> Option<T> {
+        if let Some(entry) = self.id_to_entry.remove(&id.value) {
             self.entry_to_id.remove(&entry);
+            return Some(Arc::try_unwrap(entry).unwrap());
         }
+
+        None
     }
 
-    pub fn size(&self) -> usize {
-        self.id_to_entry.len()
+    pub fn size(&self) -> u32 {
+        self.id_to_entry.len().try_into().unwrap()
     }
 }
 
-impl<T: Eq + std::hash::Hash> Default for Pool<T> {
+impl<I: PartialEq, T: Eq + Debug + Hash> Default for Pool<I, T> {
     fn default() -> Self {
         Self::new()
     }
@@ -60,17 +103,30 @@ impl<T: Eq + std::hash::Hash> Default for Pool<T> {
 mod tests {
     use super::*;
 
+    #[derive(PartialEq, Debug)]
+    pub struct UniqueNameId;
+
+    #[derive(PartialEq, Debug)]
+    pub struct OtherId;
+
     #[test]
     fn test_adding_and_fetching_entries() {
-        let mut pool: Pool<String> = Pool::new();
+        let mut pool: Pool<UniqueNameId, String> = Pool::new();
         let id = pool.add("Foo::Bar#method".into());
-        let item = pool.get(id).unwrap();
+        let item = pool.get(&id).unwrap();
         assert_eq!(*item, "Foo::Bar#method".to_string());
     }
 
     #[test]
+    fn test_fetching_non_existing_entry() {
+        let pool: Pool<UniqueNameId, String> = Pool::new();
+        let id = PoolId::<UniqueNameId>::from(1);
+        assert_eq!(pool.get(&id), None);
+    }
+
+    #[test]
     fn test_adding_the_same_entry_just_returns_id() {
-        let mut pool: Pool<String> = Pool::new();
+        let mut pool: Pool<UniqueNameId, String> = Pool::new();
         let id = pool.add("Foo::Bar#method".into());
         assert_eq!(1, pool.size());
         let other_id = pool.add("Foo::Bar#method".into());
@@ -80,9 +136,10 @@ mod tests {
 
     #[test]
     fn test_removing_entries() {
-        let mut pool: Pool<String> = Pool::new();
+        let mut pool: Pool<UniqueNameId, String> = Pool::new();
         let id = pool.add("Foo::Bar#method".into());
-        pool.remove(id);
-        assert_eq!(pool.get(id), None);
+        let item = pool.remove(&id);
+        assert_eq!(pool.get(&id), None);
+        assert_eq!(item, Some("Foo::Bar#method".to_string()));
     }
 }
