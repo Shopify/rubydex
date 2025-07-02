d5f7fe5a16b3327f62df972e6dd330eef76a0a4c
Vinicius Stock <vinicius.stock@shopify.com>
2025-07-02 09:25:08 -0400

    Connect Rust to the Ruby VM through a registry

--- ext/index/index.c
+++ ext/index/index.c
@@ -1,88 +1,10 @@
 #include "ruby.h"
 #include "rustbindings.h"
-#include <stdlib.h>
-#include <string.h>
+#include "repository.h"
 
-// Store the class references
-static VALUE cRepository;
-static VALUE cEntry;
+VALUE mIndex;
 
-static void index_free(void *ptr) {
-    if (ptr) {
-        dealloc_repository((CRepository*)ptr);
-    }
-}
-
-static const rb_data_type_t index_type = {
-    "Index",
-    {0, index_free, 0,},
-    0, 0, RUBY_TYPED_FREE_IMMEDIATELY
-};
-
-static VALUE rb_index_new(VALUE klass) {
-    CRepository* index = get_repository();
-    // TODO: Need to determine a potential way to allow interaction with the index without
-    // wrapping the object
-    return TypedData_Wrap_Struct(klass, &index_type, index);
-}
-
-static VALUE rb_repository_add_entry(VALUE self, VALUE name, VALUE value) {
-    CRepository* index;
-    TypedData_Get_Struct(self, CRepository, &index_type, index);
-
-    const char *c_name = StringValueCStr(name);
-    const char *c_value = StringValueCStr(value);
-
-    repository_add_entry(index, c_name, c_value);
-    return Qnil;
-}
-
-static VALUE rb_repository_get_entry(VALUE self, VALUE name) {
-    CRepository* index;
-    TypedData_Get_Struct(self, CRepository, &index_type, index);
-
-    const char *c_name = StringValueCStr(name);
-    CEntry *c_entry = repository_get_entry(index, c_name);
-
-    if (!c_entry) {
-        return Qnil;
-    }
-
-    const char *entry_name = entry_get_name(c_entry);
-    const char *entry_value = entry_get_value(c_entry);
-
-    // Create a plain Ruby Entry object and set instance variables
-    // instead of using TypedData_Wrap_Struct
-    VALUE ruby_entry = rb_class_new_instance(0, NULL, cEntry);
-    rb_ivar_set(ruby_entry, rb_intern("@name"), entry_name ? rb_str_new_cstr(entry_name) : Qnil);
-    rb_ivar_set(ruby_entry, rb_intern("@value"), entry_value ? rb_str_new_cstr(entry_value) : Qnil);
-
-    delloc_entry(c_entry);
-
-    return ruby_entry;
-}
-
-static VALUE rb_entry_name(VALUE self) {
-    return rb_ivar_get(self, rb_intern("@name"));
-}
-
-static VALUE rb_entry_value(VALUE self) {
-    return rb_ivar_get(self, rb_intern("@value"));
-}
-
-// Initialization function for the Ruby extension
 void Init_index(void) {
-    VALUE mIndex = rb_define_module("Index");
-
-    cRepository = rb_define_class_under(mIndex, "Repository", rb_cObject);
-    rb_define_singleton_method(cRepository, "new", rb_index_new, 0);
-    rb_define_method(cRepository, "add_entry", rb_repository_add_entry, 2);
-    rb_define_method(cRepository, "get_entry", rb_repository_get_entry, 1);
-
-    cEntry = rb_define_class_under(mIndex, "Entry", rb_cObject);
-    rb_define_method(cEntry, "name", rb_entry_name, 0);
-    rb_define_method(cEntry, "value", rb_entry_value, 0);
-
-    rb_define_attr(cEntry, "name", 1, 1);
-    rb_define_attr(cEntry, "value", 1, 1);
+    mIndex = rb_define_module("Index");
+    initialize_repository(mIndex);
 }
---
+++ ext/index/repository.c
@@ -0,0 +1,61 @@
+#include "ruby.h"
+#include "rustbindings.h"
+
+static VALUE cRepository;
+
+static void repository_free(void *ptr) {
+    if (ptr) {
+        free(ptr);
+    }
+}
+
+static const rb_data_type_t repository_type = {
+    "Repository",
+    { 0, repository_free, 0 },
+    0, 0, RUBY_TYPED_FREE_IMMEDIATELY
+};
+
+static VALUE rb_repository_alloc(VALUE klass) {
+    void* repository = new_repo();
+    return TypedData_Wrap_Struct(klass, &repository_type, repository);
+}
+
+// Convert a Ruby array of strings into a double char pointer so that we can pass that to Rust. This copies the data
+// so it must be freed
+static char** str_array_to_char(VALUE array, long length) {
+    char **converted_array = malloc(length * sizeof(char*));
+
+    for (long i = 0; i < length; i++) {
+        VALUE item = rb_ary_entry(array, i);
+        const char* string = StringValueCStr(item);
+
+        converted_array[i] = malloc(strlen(string) + 1);
+        strcpy(converted_array[i], string);
+    }
+
+    return converted_array;
+}
+
+static VALUE rb_repository_index_all(VALUE self, VALUE file_paths) {
+    rb_gc_register_address(&file_paths);
+    long length = RARRAY_LEN(file_paths);
+    char **converted_file_paths = str_array_to_char(file_paths, length);
+
+    void* repository;
+    TypedData_Get_Struct(self, void*, &repository_type, repository);
+    index_all_c(repository, converted_file_paths, length);
+
+    for (long i = 0; i < length; i++) {
+        free(converted_file_paths[i]);
+    }
+    free(converted_file_paths);
+    rb_gc_unregister_address(&file_paths);
+    return Qnil;
+}
+
+void initialize_repository(VALUE mIndex) {
+    cRepository = rb_define_class_under(mIndex, "Repository", rb_cObject);
+
+    rb_define_alloc_func(cRepository, rb_repository_alloc);
+    rb_define_method(cRepository, "index_all", rb_repository_index_all, 1);
+}
