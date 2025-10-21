fe7973c0e3509c1b7a058dd4a25dc401f36cf277
Jenny Shih <jenny.shih@shopify.com>
2025-10-21 15:17:18 -0400

    Add query for content hash
    Add query to get a set of content hashes by uri from db. This will be
    used later for staleness check.

--- rust/saturn/src/model/db.rs
+++ rust/saturn/src/model/db.rs
@@ -465,4 +465,24 @@ mod tests {
         assert!(result.contains_key(&first_uri));
         assert!(result.contains_key(&second_uri));
     }
+
+    #[test]
+    fn querying_content_hashes_by_uri() {
+        let mut context = GraphTest::new();
+
+        context.index_uri("file:///first.rb", "module FirstModule; end");
+        context.index_uri("file:///second.rb", "class SecondClass; end");
+
+        let mut db = Db::new();
+        db.initialize_connection(None).unwrap();
+        assert!(db.save_full_graph(&context.graph).is_ok());
+
+        let result = db.get_all_content_hashes().unwrap();
+
+        assert_eq!(result.len(), 2);
+        let first_uri = UriId::from("file:///first.rb");
+        let second_uri = UriId::from("file:///second.rb");
+        assert!(result.contains_key(&first_uri));
+        assert!(result.contains_key(&second_uri));
+    }
 }
