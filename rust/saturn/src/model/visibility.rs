74c1adab7a4ef8779d1e6d3c5682fa33cea0728c
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2025-12-03 18:37:00 -0500

    Introduce Visibility enum
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

---
+++ rust/saturn/src/model/visibility.rs
@@ -0,0 +1,38 @@
+use core::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Visibility {
+    Public,
+    Protected,
+    Private,
+}
+
+impl Visibility {
+    /// Parse a visibility from a string.
+    ///
+    /// Valid values are `public`, `protected`, and `private`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the string is not a valid visibility
+    #[must_use]
+    pub fn from_string(str: &str) -> Self {
+        match str {
+            "public" => Self::Public,
+            "protected" => Self::Protected,
+            "private" => Self::Private,
+            _ => panic!("Invalid visibility: {str}"),
+        }
+    }
+}
+
+impl Display for Visibility {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Public => write!(f, "public"),
+            Self::Protected => write!(f, "protected"),
+            Self::Private => write!(f, "private"),
+        }
+    }
+}
