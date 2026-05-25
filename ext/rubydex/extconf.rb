28572bcbdb05d78fa573d4733f93c8d36b23dbdc
Stan Lo <stan.lo@shopify.com>
2026-05-25 22:53:00 +0100

    Package rubydex MCP executable in gem

--- ext/rubydex/extconf.rb
+++ ext/rubydex/extconf.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "mkmf"
+require "fileutils"
 require "pathname"
 
 unless system("cargo", "--version", out: File::NULL, err: File::NULL)
@@ -81,6 +82,12 @@ else
 end
 
 lib_dir = gem_dir.join("lib").join("rubydex")
+mcp_bin_dir = lib_dir.join("bin")
+FileUtils.mkdir_p(mcp_bin_dir)
+
+mcp_executable = Gem.win_platform? ? "rubydex_mcp.exe" : "rubydex_mcp"
+mcp_src = target_dir.join(mcp_executable)
+mcp_dst = mcp_bin_dir.join(mcp_executable)
 
 copy_dylib_commands = if Gem.win_platform?
   ""
@@ -107,6 +114,7 @@ new_makefile = makefile.gsub("$(OBJS): $(HDRS) $(ruby_headers)", <<~MAKEFILE.cho
   .rust_built: $(RUST_SRCS)
   \t#{cargo_command} || (echo "Compiling Rust failed" && exit 1)
   \t$(COPY) #{bindings_path} #{__dir__}
+  \t$(COPY) #{mcp_src} #{mcp_dst}
   \ttouch $@
 
   compile_rust: .rust_built
